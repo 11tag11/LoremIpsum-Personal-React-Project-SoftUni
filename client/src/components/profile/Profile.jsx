6ee887c44bb231b95f3cf5f20897a418542802b1
11tag11 <94870411+11tag11@users.noreply.github.com>
2023-11-22 12:37:56 +0200

    Update component Profile

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './Profile.module.css';
 
-const Profile = () => {
+const Profile = ( {user} ) => {
+    console.log(user);//undefined 22.11.
     return (
 
         <div className={styles.sectionSiteMain}>
@@ -14,10 +15,10 @@ const Profile = () => {
                         <img src="./assets/user_img.jpg" alt="user" />
                     </div>
                     <section className={styles.userInfo}>
-                        <p className={styles.email}>Email: blep@abv.bg.com</p>
-                        <p className={styles.username}>username: Gargamell</p>
-                        <p className={styles.createdTopics}>Created topics: 11</p>
-                        <p className={styles.comments}>Comments: 11</p>
+                        <p className={styles.email}>Email: {user?.email || 'N/A'}</p>
+                        <p className={styles.username}>Username: {user?.username || 'N/A'}</p>
+                        <p className={styles.createdTopics}>Created topics: {user?.createdTopics || 0}</p>
+                        <p className={styles.answers}>Comments: {user?.answers || 0}</p>
                     </section>
                 </div>
             </div>
