e0ea28f9aa017b59ebbd43bab40c186e05ba65a8
11tag11 <94870411+11tag11@users.noreply.github.com>
2023-11-25 18:13:10 +0200

    Update server and App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
@@ -9,40 +10,34 @@ import Register from './components/auth/Register';
 import AllTopics from './components/allTopics/AllTopics';
 import CreateTopic from './components/createTopic/CreateTopic';
 import DetailsPage from './components/detailsPage/DetailsPage';
-// import DetailsPageAnswers from './components/detailsPage/DetailsPageAnswers';
 
 import Profile from './components/profile/Profile';
 import PageNotFound from './components/pageNotFound/PageNotFound';
-
-
 
 import styles from './components/styles.module.css';
 // import Loader from './components/shared/Loader';
 
 function App() {
   return (
-    <div>
-      <div className={styles.siteWrapper}>
-        <Header />
-
-        {/* <Loader /> */}
-
-        <Routes>
-          <Route path="/latestTopics" element={<LatestTopics />} />
-          <Route path='/allTopics' element={<AllTopics />} />
-          <Route path='/createTopic' element={<CreateTopic />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/profile' element={<Profile />} />
+    <AuthProvider>
+        <div className={styles.siteWrapper}>
+          <Header />
+          {/* <Loader /> */}
+          <Routes>
+            <Route path="/latestTopics" element={<LatestTopics />} />
+            <Route path='/allTopics' element={<AllTopics />} />
+            <Route path='/createTopic' element={<CreateTopic />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/profile' element={<Profile />} />
             {/* ?? */}
-          <Route path='/latestTopics/:topicId' element={<DetailsPage />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-        
-        <Footer />
-      </div>
-    </div>
+            <Route path='/details/:topicId' element={<DetailsPage />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
