1495292c8469bcc9e5c2297224bc4fda65b00f1b
11tag11 <94870411+11tag11@users.noreply.github.com>
2023-11-28 17:05:25 +0200

    Add Components

---
+++ client/src/components/deleteAnswer/DeleteAnswer.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as answerService from '../../services/answerService';
+import styles from './DeleteAnswer.module.css';
+
+const DeleteAnswer = ({answerId, onClose, topicId}) => {
+    const navigate = useNavigate();
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    const handleDelete = async () => {
+        try {
+            setIsDeleting(true);
+            await answerService.remove(answerId);
+            onClose();
+            navigate(`/details/${topicId}`);
+        } catch (error) {
+            console.error('Error deleting answer', error);
+        } finally {
+            setIsDeleting(false);
+        }
+    };
+
+    return (
+        <div className={`${styles.modalOverlay} ${isDeleting ? 'show' : ''}`}>
+          <div className={styles.modal}>
+            <div className={styles.modalContent}>
+              <span className={styles.close} onClick={onClose}>&times;</span>
+              <h2>Confirm Deletion</h2>
+              <p>Are you sure you want to delete your answer?</p>
+              <div className={styles.modalButtons}>
+                <button className={styles.deleteButton}
+                onClick={handleDelete} disabled={isDeleting}>
+                  {isDeleting ? 'Deleting...' : 'Delete'}
+                </button>
+                <button className={styles.cancelButton}
+                onClick={onClose} disabled={isDeleting}>Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+};
+
+export default DeleteAnswer;
---
+++ client/src/components/detailsPage/EditAnswer.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from '../../contexts/AuthContext';
+import { formatDate } from "../../utils/dateUtils";
+import * as answerService from '../../services/answerService';
+import styles from './EditAnswer.module.css';
+
+const EditAnswer = () => {
+    const { auth } = useContext(AuthContext);
+    const { answerId } = useParams();
+    const [answer, setAnswer] = useState('');
+    const [topicId, setTopicId] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log('Auth object:', auth);
+        // Fetch the existing topic data and set it to the state
+        answerService.getAnswerById(answerId)
+            .then(result => {
+                setAnswer(result.answer);
+                setTopicId(result.topicId);
+            })
+            .catch(error => console.error('Error fetching topic:', error));
+    }, [answerId, auth._id]);
+
+    const resetEditForm = () => {
+        setAnswer('');
+    };
+
+    const answerChangeHandler = (e) => {
+        setAnswer(e.target.value);
+    };
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const currentDate = new Date();
+        const formattedDate = formatDate(currentDate);
+
+        const updatedAnswerData = {
+            answer,
+            _updatedOn: formattedDate,
+            username: auth.username,
+            topicId: topicId,
+            userId: auth._id
+        };
+
+        answerService.editAnswer(answerId, updatedAnswerData)
+            .then(() => navigate(`/details/${topicId}`))
+            .catch(error => console.log('Answer did not update!', error));
+
+        resetEditForm();
+    };
+
+    return (
+        <div className="section-site-main">
+            <div className={styles.containerCreate}>
+                <div className={styles.newPostHeading}>
+                    <h1 className={styles.newHeading}>Edit Answer</h1>
+                </div>
+                <div className={styles.sectionArticles}>
+                    <div className={styles.sectionArticle}>
+                        <section className="new-topic-form">
+
+                            <form className={styles.createForm} action="#" method="">
+                                <p className={styles.specific}>Be specific and imagine you’re asking a question to another person, e.g. What are React Hooks good for?</p>
+                                
+                                <textarea
+                                    type="text"
+                                    name="answer"
+                                    id="answer"
+                                    value={answer}
+                                    onChange={answerChangeHandler}
+                                    cols={30}
+                                    rows={10}
+                                    placeholder="Topic"
+                                />
+                                <div className={styles.postButtonContainer}>
+                                    <button
+                                        type="button"
+                                        className={styles.newPostButton}
+                                        onClick={submitHandler}>Save</button>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default EditAnswer;
