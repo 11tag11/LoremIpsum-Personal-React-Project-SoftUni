4e792b076ee7589c350e168156aa841458ba5de3
11tag11 <94870411+11tag11@users.noreply.github.com>
2023-12-06 22:52:46 +0200

    Update Service and hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,3 +1,6 @@
+
+
+
 import { useState, useEffect } from 'react';
 
 export default function useForm(submitHandler, initialValues) {
--- client/src/services/answerService.js
+++ client/src/services/answerService.js
@@ -27,18 +27,30 @@ export const createAnswer = async (topicId, answer, auth, accessToken) => {
 
 export const getAnswersForTopic = async (topicId) => {
   const result = await request.get(`${baseUrl}?topicId=${topicId}`);
-
+console.log('OOO',result);
+console.log('OOO',topicId);
   // Filter answers based on the topicId
   const filteredAnswers = Object.values(result)
     .filter(answer => answer.topicId === topicId);
 
-  // console.log('Filtered Answers for Topic:', filteredAnswers);
+  console.log('Filtered Answers for Topic:', filteredAnswers);
   return filteredAnswers;
 };
 
-export const getAnswerById = async (answerId) => {
-  const result = await request.get(`${baseUrl}/${answerId}`);
-  // console.log(answerId);
+export const getAnswersForUser = async (userId) => {
+  const result = await request.get(`${baseUrl}?userId=${userId}`);
+  return result;
+};
+
+// export const getAnswerById = async (userId) => {
+//   const result = await request.get(`${baseUrl}/${userId}`);
+//   console.log(userId);
+//   return result;
+// };
+
+export const getAnswerById = async (userId) => {
+  const result = await request.get(`${baseUrl}/${userId}`);
+  console.log('eee',userId);
   return result;
 };
 
