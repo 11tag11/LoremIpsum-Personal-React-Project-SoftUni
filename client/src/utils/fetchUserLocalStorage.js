33f98d03f21f30f1e91160fc3e916753de1f27ad
11tag11 <94870411+11tag11@users.noreply.github.com>
2023-12-08 17:20:02 +0200

    Delete fetchUserLocalStorage.js

--- client/src/utils/fetchUserLocalStorage.js
+++
@@ -1,7 +0,0 @@
-export const fetchUser = () => {
-    const userInfo = localStorage.getItem('email') !== "undefined"
-     ? JSON.parse(localStorage.getItem('email')) 
-     : localStorage.clear(); 
-     console.log(userInfo);
-    return userInfo;
-    }
